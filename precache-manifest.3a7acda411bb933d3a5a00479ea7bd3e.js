4765748344ff8e5ad47ec2b34e3e73032e6315c5
danich1 <danich1@users.noreply.github.com>
2020-08-19 18:05:49 +0000

    deploy: 9aa02f26e4279d53015c0586af84651ac02dacc5

---
+++ precache-manifest.3a7acda411bb933d3a5a00479ea7bd3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d88a6a805e9520b452502cdda6ae2d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "484811eb8a828a25d95a",
+    "url": "./static/css/main.74dc2a6e.chunk.css"
+  },
+  {
+    "revision": "f56c66d9abb8e9841dac",
+    "url": "./static/js/2.c3667686.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c3667686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484811eb8a828a25d95a",
+    "url": "./static/js/main.53738bea.chunk.js"
+  },
+  {
+    "revision": "a0b3c4a99ef04e206b58",
+    "url": "./static/js/runtime-main.fdc69087.js"
+  },
+  {
+    "revision": "e444815ccb02a6a676eb40f62632ce96",
+    "url": "./static/media/logo.e444815c.svg"
+  }
+]);
